93bcd7b19de62625b02ef99828792125272fe84a
Guy Carpenter <guy.carpenter@simplehq.com.au>
2016-09-11 20:47:42 +1000

    Add Android support for name parameter, addressing issue #31

--- android/src/main/java/com/yoloci/fileupload/FileUploadModule.java
+++ android/src/main/java/com/yoloci/fileupload/FileUploadModule.java
@@ -109,11 +109,12 @@ public class FileUploadModule extends ReactContextBaseJavaModule {
                 ReadableMap file = files.getMap(i);
                 String filename = file.getString("filename");
                 String filepath = file.getString("filepath");
+                String name = file.hasKey("name") ? file.getString("name") : filename;
                 filepath = filepath.replace("file://", "");
                 fileInputStream = new FileInputStream(filepath);
 
                 outputStream.writeBytes(twoHyphens + boundary + lineEnd);
-                outputStream.writeBytes("Content-Disposition: form-data; name=\"image\";filename=\"" + filename + "\"" + lineEnd);
+                outputStream.writeBytes("Content-Disposition: form-data; name=\"" + name + "\";filename=\"" + filename + "\"" + lineEnd);
                 outputStream.writeBytes(lineEnd);
 
                 bytesAvailable = fileInputStream.available();
